13adaf827065af12b140301ff32b5177aa11a125
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-01-22 16:30:18 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,6 +3,10 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        from cloudlink import cloudlink
+        cl = cloudlink()
+        server = cl.server(logs=False)
+        server.run(ip = "0.0.0.0",port = 3000)
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
